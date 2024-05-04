8ed9eaad3bdf7d00e79742ca60248c0e388a12ce
Cris9898 <157153132+Cris9898@users.noreply.github.com>
2024-05-04 00:13:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,19 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let dominios = document.getElementById("dominios");
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        console.log(pronoun + adj + noun + ".com");
+        dominios.innerHTML += pronoun + adj + noun + ".com";
+      }
+    }
+  }
 };
